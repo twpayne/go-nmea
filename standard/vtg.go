27ad52081455af7e79db7f7bf590f0257ece2c8e
Tom Payne <twpayne@gmail.com>
2024-04-09 19:15:10 +0200

    feat: handle missing values in VTG format

--- standard/vtg.go
+++ standard/vtg.go
@@ -4,20 +4,20 @@ import "github.com/twpayne/go-nmea"
 
 type VTG struct {
 	nmea.Address
-	TrueCourseOverGround     float64
+	TrueCourseOverGround     nmea.Optional[float64]
 	MagneticCourseOverGround nmea.Optional[float64]
-	SpeedOverGroundKN        float64
-	SpeedOverGroundKPH       float64
+	SpeedOverGroundKN        nmea.Optional[float64]
+	SpeedOverGroundKPH       nmea.Optional[float64]
 	ModeIndicator            byte
 }
 
 func ParseVTG(addr string, tok *nmea.Tokenizer) (*VTG, error) {
 	var vtg VTG
 	vtg.Address = nmea.NewAddress(addr)
-	vtg.TrueCourseOverGround = tok.CommaFloatCommaUnit('T')
+	vtg.TrueCourseOverGround = tok.CommaOptionalFloatCommaUnit('T')
 	vtg.MagneticCourseOverGround = tok.CommaOptionalFloatCommaUnit('M')
-	vtg.SpeedOverGroundKN = tok.CommaFloatCommaUnit('N')
-	vtg.SpeedOverGroundKPH = tok.CommaFloatCommaUnit('K')
+	vtg.SpeedOverGroundKN = tok.CommaOptionalFloatCommaUnit('N')
+	vtg.SpeedOverGroundKPH = tok.CommaOptionalFloatCommaUnit('K')
 	vtg.ModeIndicator = tok.CommaOneByteOf("ADEFNR")
 	tok.EndOfData()
 	return &vtg, tok.Err()
